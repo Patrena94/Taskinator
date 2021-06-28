9e77448e4ff042e42df0ebd70448fd8be7c0c85b
Vashti <patrena@sbcglobal.net>
2021-06-28 12:11:07 -0700

    updated input

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -3,14 +3,16 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function(event){
     event.preventDefault();
-
+var taskNameInput = doucment.querySelector("input[name='task-name']").value;
+var taskTypeInput=doucment.querySelector("select[name='task-type']").value;
 var listItemEl = document.createElement("li");
 listItemEl.className = "task-item";
-listItemEl.textContent = "This is a new task.";
+var taskInfoEl = document.createElement("div");
+taskInfoEl.className="task-info";
+taskInfoEl.innerHTML = "<h3 class ='task-name'>" + taskDataObj.name +"</h3><span class ='task-type'>" + taskDataObj.type + "</span>";
 tasksToDoEl.appendChild(listItemEl);
 };
 formEl.addEventListener("submit", createTaskHandler);
-
 
 
 
