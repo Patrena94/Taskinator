c928e3a98d1775ee6b8dd53f17959cbcf6447172
Vashti <patrena@sbcglobal.net>
2021-06-28 13:11:49 -0700

    form final update

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,7 +1,36 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function(event){
+var taskFormHandler =function(event){
+    event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput=doucment.querySelector("select[name='task-type']").value;
+// package up data as an object
+var taskDataObj ={
+    name: taskNameInput,
+    type: taskTypeInput
+};
+//send it as an argument to createTaskEL
+createTaskEl(taskDataObj);
+};
+
+var createTaskEl = function (taskDataObj){
+    //create list item 
+    var listItemEl = document.createElement("li");
+ listItemEl.className = "task-item";
+
+ //create div to hold task info and add to list item
+ var taskInfoEl = document.createElement("div");
+ taskInfoEl.className="task-info";
+ taskInfoEl.innerHTML = "<h3 class ='task-name'>" + taskDataObj.name +"</h3><span class ='task-type'>" + taskDataObj.type + "</span>";
+ listItemEl.appendChild(taskInfoEl);
+ 
+ //add entire list item to list
+ tasksToDoEl.appendChild(listItemEl);
+  };
+  formEl.addEventListener("submit", createTaskHandler);
+
+  var createTaskHandler = function(event){
     event.preventDefault();
 var taskNameInput = doucment.querySelector("input[name='task-name']").value;
 var taskTypeInput=doucment.querySelector("select[name='task-type']").value;
@@ -10,9 +39,16 @@ listItemEl.className = "task-item";
 var taskInfoEl = document.createElement("div");
 taskInfoEl.className="task-info";
 taskInfoEl.innerHTML = "<h3 class ='task-name'>" + taskDataObj.name +"</h3><span class ='task-type'>" + taskDataObj.type + "</span>";
+listItemEl.appendChild(taskInfoEl);
+//add entire list item to list
 tasksToDoEl.appendChild(listItemEl);
 };
-formEl.addEventListener("submit", createTaskHandler);
+
+ // check if input values are empty strings
+ if(!taskNameInput || !taskTypeInput) {alert("You need to fill out the taskform!");
+return false;
+}
+
 
 
 
